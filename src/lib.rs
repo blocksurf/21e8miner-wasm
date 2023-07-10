f058412574c20b2d3b4694246191ca9d4ad77d58
blocksurf <trust@hepburn.cc>
2023-07-10 17:59:25 -0400

    add binaries

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,12 @@ pub use miner::*;
 
 pub mod config;
 pub use crate::config as miner_config;
+pub use miner_config::*;
+
+pub fn start() {
+    MagicMiner::start()
+}
+
+pub fn setup() {
+    MinerConfig::start()
+}
